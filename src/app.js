8a7edef84f902bb45b8d380fac7f258c4397399f
surajbhatt27 <bhattsuraj0066@gmail.com>
2025-05-08 16:33:11 +0530

    feat: add CORS and cookie-parser middleware, update environment variables, and create user routes file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+app.use(
+    cors({
+        origin:process.env.CORS_ORIGIN,
+        Credential: true
+    })
+)
+
+// middlwares
+
+app.use(express.json({limit: "16kb"}))   // Parse json bodies
+app.use(express.urlencoded({extended: true, limit: "16kb"}))   // Parse form data
+app.use(cookieParser())    //Parse cookies
 
 export {app}
